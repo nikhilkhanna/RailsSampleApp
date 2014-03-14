8eff75055dca0dc0b39b039500c531a57a8190c5
nikhilkhanna <nikhilkh@gmail.com>
2014-03-14 11:41:19 -0700

    Add full_title helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+	#returns full title based on the page
+
+	def full_title(page_title)
+		base_title = "Khanna Sample"
+		if page_title.empty?
+			base_title
+		else
+			"#{base_title} | #{page_title}"
+		end
+	end
 end
